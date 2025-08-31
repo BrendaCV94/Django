c9f9fded68aada7cbcb3831b129052478044c537
Brenda <brendacarrizovega@gmail.com>
2025-08-31 20:15:55 -0300

    Ultimo

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-
-from inicio.views import inicio
+from inicio.views import inicio, crear_auto_v2, listado_autos #, crear_auto_v1
 
 urlpatterns = [
-    path('', inicio, name='vista')
+    path('', inicio, name='inicio'),
+    path('autos/', listado_autos, name='listado_autos'),
+    path('autos/crear', crear_auto_v2, name='crear_auto'),
+      # path('crear-auto/<marca>/<modelo>/', crear_auto_v1, name='crear_auto'),
 ]
+
